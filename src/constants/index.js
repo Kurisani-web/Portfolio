d4d0de11690226665a0201c1b173f8e935a9f781
Kurisani-web <kurisanim2@gmail.com>
2025-04-27 08:01:27 +0000

    new

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,9 +29,9 @@ const words = [
 ];
 
 const counterItems = [
-  { value: 15, suffix: "+", label: "Years of Experience" },
-  { value: 200, suffix: "+", label: "Satisfied Clients" },
-  { value: 108, suffix: "+", label: "Completed Projects" },
+  { value: 3, suffix: "+", label: "Years of Experience" },
+  { value: 180, suffix: "+", label: "Satisfied Clients" },
+  { value: 105, suffix: "+", label: "Completed Projects" },
   { value: 90, suffix: "%", label: "Client Retention Rate" },
 ];
 
@@ -171,7 +171,7 @@ const expCards = [
     ],
   },
   {
-    review: "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
+    review: "Kurisani’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
     imgPath: "/images/exp3.png",
     logoPath: "/images/logo3.png",
     title: "React Native Developer",
@@ -232,7 +232,7 @@ const testimonials = [
     name: "Floyd Miles",
     mentions: "@floydmiles",
     review:
-      "Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
+      "Kurisani’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
     imgPath: "/images/client4.png",
   },
   {
---
+++ src/stack/client.js
@@ -0,0 +1,9 @@
+import { StackClientApp } from "@stackframe/js";
+
+export const stackClientApp = new StackClientApp({
+  tokenStore: "cookie",
+
+  // get your Stack Auth API keys from https://app.stack-auth.com and store them in a safe place (eg. environment variables)
+  publishableClientKey: process.env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY,
+  projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID,
+});
